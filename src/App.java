a5f4ba6b7e6631a053925fd580cce956ec5ef481
Garik073 <gariklukin073@gmail.com>
2023-03-12 20:57:23 +0300

    HomeWork2.2

--- src/App.java
+++ src/App.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.CloseAction;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите четное число: " );
+        System.out.println("Введите четное число:  " );
         int n = in.nextInt();
         System.out.println("Введите первый символ с1: " );
         char c1 = in.next().charAt(0);
@@ -17,5 +25,19 @@ public class App {
               s.setCharAt(2 * i + 1, c2);
         }
         System.out.println(s.toString());
+        NewRecord();
+    }
+
+    private static void NewRecord() throws IOException{
+        System.out.println(File.separator);
+        FileOutputStream fileOutputStream = new FileOutputStream("file.txt");
+        
+        String text = "TEXT \n";
+        text =text.repeat(100);
+        
+        byte [] bytes = text.getBytes(StandardCharsets.UTF_8);
+        fileOutputStream.write(bytes);
+        fileOutputStream.close();
+        
     }
 }
